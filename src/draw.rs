e634d45c3e0d500f9e13478142d338cd32ab15c7
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2023-06-25 19:18:18 +0800

    Refactor...

--- src/draw.rs
+++ src/draw.rs
@@ -3,17 +3,44 @@ use std::{
     io::{stdout, Stdout},
 };
 
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use crossterm::{cursor, style::Print, terminal, ExecutableCommand};
 
 use crate::position::Coordinates;
 
-pub struct Draw;
+pub struct Draw {
+    stdout: Stdout,
+}
 
 impl Draw {
-    pub fn draw_snake(stdout: &mut Stdout, snake_body: &Vec<Coordinates>, body_style: Cow<&str>) {
+    pub fn init() -> Result<Self, Box<dyn std::error::Error>> {
+        // Need to have this for this to work on Linux environment
+        enable_raw_mode()?;
+        let mut stdout = stdout();
+        // Clear the terminal and hide the cursor before starting the game
+        stdout
+            .execute(terminal::Clear(terminal::ClearType::All))?
+            .execute(cursor::Hide)?
+            .execute(cursor::EnableBlinking)?;
+
+        Ok(Self { stdout })
+    }
+
+    pub fn deinit(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        // Clear the terminal and show the cursor back before exiting the game
+        self.stdout
+            .execute(crossterm::cursor::MoveTo(0, 0))?
+            .execute(terminal::Clear(terminal::ClearType::All))?
+            .execute(cursor::Show)?;
+        // Need to have this for this to work on Linux environment
+        disable_raw_mode()?;
+        Ok(())
+    }
+
+    pub fn draw_snake(&mut self, snake_body: &Vec<Coordinates>, body_style: Cow<&str>) {
         let mut i = 0;
         while i < snake_body.len() {
-            stdout
+            self.stdout
                 .execute(crossterm::cursor::MoveTo(snake_body[i].x, snake_body[i].y))
                 .unwrap()
                 .execute(Print(&body_style))
@@ -23,16 +50,16 @@ impl Draw {
         }
     }
 
-    pub fn draw_food(stdout: &mut Stdout, food_position: &Coordinates, food_style: Cow<&str>) {
-        stdout
+    pub fn draw_food(&mut self, food_position: &Coordinates, food_style: Cow<&str>) {
+        self.stdout
             .execute(crossterm::cursor::MoveTo(food_position.x, food_position.y))
             .unwrap()
             .execute(Print(&food_style))
             .unwrap();
     }
 
-    pub fn remove_snake_trail(stdout: &mut Stdout, body_trail: &Coordinates) {
-        stdout
+    pub fn remove_snake_trail(&mut self, body_trail: &Coordinates) {
+        self.stdout
             .execute(crossterm::cursor::MoveTo(body_trail.x, body_trail.y))
             .unwrap()
             .execute(Print(" "))
@@ -40,70 +67,70 @@ impl Draw {
     }
 
     pub fn draw_board(
-        stdout: &mut Stdout,
+        &mut self,
         upper_left: &Coordinates,
         bottom_right: &Coordinates,
     ) -> Result<(), Box<dyn std::error::Error>> {
-        stdout
+        self.stdout
             .execute(crossterm::cursor::MoveTo(upper_left.x, 1))?
             .execute(Print("╔"))?;
 
-        stdout
+        self.stdout
             .execute(crossterm::cursor::MoveTo(bottom_right.x, 1))?
             .execute(Print("╗"))?;
 
-        stdout
+        self.stdout
             .execute(crossterm::cursor::MoveTo(upper_left.x, 2))?
             .execute(Print("║"))?;
 
-        stdout
+        self.stdout
             .execute(crossterm::cursor::MoveTo(bottom_right.x, 2))?
             .execute(Print("║"))?;
 
         for x in (upper_left.x + 1)..bottom_right.x {
-            stdout
+            self.stdout
                 .execute(crossterm::cursor::MoveTo(x, 1))?
                 .execute(Print("═"))?;
         }
 
-        stdout
+        self.stdout
             .execute(crossterm::cursor::MoveTo(2, 2))?
             .execute(Print(
                 " Quit(Esc)     Up(↑)     Down(↓)     Left(←)     Right(→)",
             ))?;
 
-        stdout
+        self.stdout
             .execute(crossterm::cursor::MoveTo(upper_left.x, upper_left.y))?
             .execute(Print("╠"))?;
 
-        stdout
+        self.stdout
             .execute(crossterm::cursor::MoveTo(bottom_right.x, upper_left.y))?
             .execute(Print("╣"))?;
 
-        stdout
+        self.stdout
             .execute(crossterm::cursor::MoveTo(upper_left.x, bottom_right.y))?
             .execute(Print("╚"))?;
 
-        stdout
+        self.stdout
             .execute(crossterm::cursor::MoveTo(bottom_right.x, bottom_right.y))?
             .execute(Print("╝"))?;
 
         for y in (upper_left.y + 1)..bottom_right.y {
-            stdout
+            self.stdout
                 .execute(crossterm::cursor::MoveTo(upper_left.x, y))?
                 .execute(Print("║"))?;
 
-            stdout
+            self.stdout
                 .execute(crossterm::cursor::MoveTo(bottom_right.x, y))?
                 .execute(Print("║"))?;
         }
 
         for x in (upper_left.x + 1)..bottom_right.x {
-            stdout
+            self.stdout
                 .execute(crossterm::cursor::MoveTo(x, upper_left.y))?
                 .execute(Print("═"))?;
 
-            stdout
+            self.stdout
                 .execute(crossterm::cursor::MoveTo(x, bottom_right.y))?
                 .execute(Print("═"))?;
         }
@@ -112,32 +139,12 @@ impl Draw {
             "Lorenzo Leonardo's Snake Game Cross-Platform Terminal Using Rust-Lang (c) 2023"
                 .to_string();
 
-        stdout
+        self.stdout
             .execute(crossterm::cursor::MoveTo(
                 (bottom_right.x / 2) - (label.len() as u16 / 2),
                 bottom_right.y + 1,
             ))?
             .execute(Print(label))?;
-        Ok(())
-    }
-
-    pub fn initialize_terminal() -> Result<Stdout, Box<dyn std::error::Error>> {
-        let mut stdout = stdout();
-        // Clear the terminal and hide the cursor before starting the game
-        stdout
-            .execute(terminal::Clear(terminal::ClearType::All))?
-            .execute(cursor::Hide)?
-            .execute(cursor::EnableBlinking)?;
-
-        Ok(stdout)
-    }
-
-    pub fn restore_terminal(mut stdout: Stdout) -> Result<(), Box<dyn std::error::Error>> {
-        // Clear the terminal and show the cursor back before exiting the game
-        stdout
-            .execute(crossterm::cursor::MoveTo(0, 0))?
-            .execute(terminal::Clear(terminal::ClearType::All))?
-            .execute(cursor::Show)?;
         Ok(())
     }
 }
--- src/engine.rs
+++ src/engine.rs
@@ -3,7 +3,6 @@
  * Date : September 15, 2022
  */
 // Standard libraries
-use std::io::Stdout;
 use std::time::Duration;
 // 3rd party crates
 use crossterm::event::KeyCode;
@@ -24,12 +23,13 @@ pub struct GameEngine {
 
 impl GameEngine {
     pub fn new(
-        upper_left: Coordinates,
-        bottom_right: Coordinates,
-        dir: SnakeDirection,
         rx_key_event: UnboundedReceiver<KeyCode>,
         tx_snake_died: UnboundedSender<bool>,
     ) -> Self {
+        // Initialize the board size
+        let upper_left = Coordinates::new(1, 3);
+        let bottom_right = Coordinates::new(120, 37);
+        let dir = SnakeDirection::Right;
         Self {
             upper_left,
             bottom_right,
@@ -61,56 +61,67 @@ impl GameEngine {
     /// The game loop
     pub async fn run(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         // Initialization
-        let mut stdout = Draw::initialize_terminal()?;
-
-        Draw::draw_board(&mut stdout, &self.upper_left, &self.bottom_right)?;
-
+        let mut draw = Draw::init()?;
         let mut snake = Snake::new(self.upper_left, self.bottom_right);
         let mut food = Food::new(self.upper_left, self.bottom_right);
         let delay = Duration::from_millis(30);
 
-        food.create_food(&snake.snake_body);
-
+        // Initialize
+        self.initialize(&mut draw, &mut snake, &mut food)?;
+        // game loop
         while snake.is_alive {
             // input
             self.dir = self.listen_for_key_press();
             if self.dir == SnakeDirection::Esc {
                 break;
             }
-
             // update
-            if snake.head == food.food_position {
-                snake.grow_snake(food.food_position);
-                food.create_food(&snake.snake_body);
-            }
-
+            self.update(&mut snake, &mut food);
             // render
-            self.draw_snake(&mut snake, self.dir, &mut stdout);
-            self.draw_food(&mut food, &mut stdout);
+            self.render(&mut snake, &mut food, &mut draw);
 
             tokio::time::sleep(delay).await;
         }
-
         // Shutdown
-        if let Err(_err) = self.tx_snake_died.send(true) {}
-
-        Draw::restore_terminal(stdout)
+        self.shutdown(&mut draw)
     }
 
-    fn draw_snake(&mut self, snake: &mut Snake, dir: SnakeDirection, stdout: &mut Stdout) {
+    fn render(&self, snake: &mut Snake, food: &mut Food, draw: &mut Draw) {
         snake.remove_trail(|body_trail| {
-            Draw::remove_snake_trail(stdout, body_trail);
+            draw.remove_snake_trail(body_trail);
         });
-        snake.set_direction(dir);
-        snake.crawl_snake();
         snake.display_snake(|snake_body| {
-            Draw::draw_snake(stdout, snake_body, std::borrow::Cow::Owned("█"));
+            draw.draw_snake(snake_body, std::borrow::Cow::Owned("█"));
+        });
+        food.display_food(|food_position| {
+            draw.draw_food(food_position, std::borrow::Cow::Owned("@"));
         });
     }
 
-    fn draw_food(&mut self, food: &mut Food, stdout: &mut Stdout) {
-        food.display_food(|food_position| {
-            Draw::draw_food(stdout, food_position, std::borrow::Cow::Owned("@"));
-        });
+    fn update(&mut self, snake: &mut Snake, food: &mut Food) {
+        if snake.head == food.food_position {
+            snake.grow_snake(food.food_position);
+            food.create_food(&snake.snake_body);
+        }
+        snake.set_direction(self.dir);
+        snake.crawl_snake();
+    }
+
+    fn initialize(
+        &mut self,
+        draw: &mut Draw,
+        snake: &mut Snake,
+        food: &mut Food,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        draw.draw_board(&self.upper_left, &self.bottom_right)?;
+        food.create_food(&snake.snake_body);
+        Ok(())
+    }
+
+    fn shutdown(&mut self, draw: &mut Draw) -> Result<(), Box<dyn std::error::Error>> {
+        if let Err(err) = self.tx_snake_died.send(true) {
+            eprintln!("Error: {:?}", err);
+        }
+        draw.deinit()
     }
 }
